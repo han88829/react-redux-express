c227dc70b5aa3014c0182e884be96cec4e8bc229
Han <h88829@foxmail.com>
2018-10-08 20:39:58 +0800

    antd 配置更改

--- view/config-overrides.js
+++ view/config-overrides.js
@@ -1,4 +1,9 @@
+const { injectBabelPlugin } = require("react-app-rewired");
+
 module.exports = function override(config, env) {
-    // do stuff with the webpack config...
-    return config;
-  };
+  config = injectBabelPlugin(
+    ["import", { libraryName: "antd", libraryDirectory: "es", style: "css" }],
+    config
+  );
+  return config;
+};
