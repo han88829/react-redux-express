cbbb5dc3714e714577643c092128710b05d8a47f
Han <h88829@foxmail.com>
2018-10-10 08:09:07 +0800

    已修改        view/config-overrides.js 已修改        view/package.json 缺失         view/src/App.css 缺失         view/src/App.js 缺失         view/src/App.test.js 缺失         view/src/index.css 已修改        view/src/index.js 缺失         view/src/logo.svg 缺失         view/src/serviceWorker.js 已修改        view/yarn.lock

--- view/src/index.js
+++ view/src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import RouteComponent from "./Route";
+import { createStore, applyMiddleware, compose } from "redux";
+import counter, { onAdd, onDecrease } from "./index.redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 配合chrome调试工具
+const reduxDevTools = window.devToolsExtension
+  ? window.devToolsExtension()
+  : () => {};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(
+  counter,
+  compose(
+    applyMiddleware(thunk),
+    reduxDevTools
+  )
+);
+
+ReactDom.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">首页</Link>
+        </li>
+        <li>
+          <Link to="/else">其他</Link>
+        </li>
+        <li>
+          <Link to="/404">404</Link>
+        </li>
+        <Switch>
+          <Route path="/" exact component={RouteComponent} />
+          <Route path="/else" component={() => "其他页面"} />
+          <Route component={() => "404"} />
+        </Switch>
+      </ul>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
